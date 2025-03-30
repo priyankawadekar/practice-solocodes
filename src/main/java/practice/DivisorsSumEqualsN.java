890c7acd233b234b4aefd2cb39c2a82be0668f94
priyanka.wadekar1@gmail.com <Admin@DESKTOP-LJ8DN0K.mshome.net>
2025-03-30 13:10:07 +0530

    barclays hackerearth 1 q empty class

---
+++ src/main/java/practice/DivisorsSumEqualsN.java
@@ -0,0 +1,29 @@
+package practice;
+
+/**
+ * <br>
+ * <b>Input</b> - <br>
+ * all integers separated by space <br>
+ * <b>Output</b> - <br>
+ * if integer is equal to sum of all its divisors excluding itself then Y else
+ * N. <br>
+ * Example - <br>
+ * Input - 6 28 15 <br>
+ * Output - Y Y N <br>
+ * 6 divisors are 1,2,3. 1+2+3 = 6. So output Y. <br>
+ * 28 divisors are 1,2,4,7,14. 1+2+4+7+14 = 28. So output Y. <br>
+ * 15 divisors are 1,3,5. 1+3+5=9 !=15. So output N. 
+ * 
+ * @implNote
+ * 1. impl using regular coding.<br>
+ * 2. impl using functional programming and collection stream based handling
+ * 
+ */
+public class DivisorsSumEqualsN {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
